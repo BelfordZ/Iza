20429ebe7fb98e43e9573f94bebe51a4d2fb60e1
Zachary Alexander Belford <belfordz66@gmail.com>
2014-04-28 16:39:26 -0700

    added test case, removed new RegExp due to some weirdness

--- src/spec/email.js
+++ src/spec/email.js
@@ -1,20 +1,30 @@
 var tape = require('tape');
 var iza = require('../lib/iza.js');
 
+var shouldBeTrue = [
+  "belfordz66@gmail.com",
+  "jaime+testaccount@battlefy.com"
+];
 
-tape("email", function(t) {
+var shouldBeFalse = [
+  "sadasdsa@asdkaKsd sdfsdf df"
+];
 
-  var shouldBeTrue = [
-    "belfordz66@gmail.com",
-    "jaime+testaccount@battlefy.com"
-  ];
 
+tape("email that should work", function(t) {
   t.plan(shouldBeTrue.length);
 
   for (var i = 0; i < shouldBeTrue.length; i++) {
     var valid = iza.email(shouldBeTrue[i]);
     t.equal(valid, true);
+  }  
+});
+
+tape("email that should not work", function(t) {
+  t.plan(shouldBeFalse.length);
+
+  for (var i = 0; i < shouldBeFalse.length; i++) {
+    var valid = iza.email(shouldBeFalse[i]);
+    t.equal(valid, false);
   }
-  
-  
 });
