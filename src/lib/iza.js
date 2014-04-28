95c2ec837a0ef1f53e9ed10c2e65ee012c124f0e
Zachary Alexander Belford <belfordz66@gmail.com>
2014-04-28 12:30:17 -0700

    first

---
+++ src/lib/iza.js
@@ -0,0 +1,10 @@
+var regex = {
+  email: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+};
+
+module.exports = {
+  email: function(strToTest) {
+    if (typeof strToTest !== 'string') { return false; }
+    return regex.email.test(strToTest);
+  }
+};
