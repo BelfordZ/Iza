06b161235b4003e69dbaccfab1fb4e8aa43920a8
Zachary Alexander Belford <belfordz66@gmail.com>
2014-04-28 12:43:32 -0700

    compile regex via RegExp constructor since jareds syntax higlighting is fucked up

---
+++ src/spec/email.js
@@ -0,0 +1,14 @@
+var tape = require('tape');
+var iza = require('../lib/iza.js');
+
+
+tape("email", function(t) {
+
+  var toTest = [
+    "belfordz66@gmail.com",
+    
+  ];
+
+  iza.email();
+  
+});
